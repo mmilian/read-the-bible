74fdba7c52c7e7ead82c5df873ca3c96108b4e9f
mmilian <mateusz.milian@gmail.com>
2024-01-27 23:41:06 +0100

    Add prefetching

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const withSerwist = require("@serwist/next").default({
     swSrc: "src/app/sw.ts",
     swDest: "public/sw.js",
+    cacheOnFrontEndNav: true,
 });
 
 
---
+++ src/app/build.js
@@ -0,0 +1,12 @@
+import { injectManifest } from "@serwist/build";
+// Build something...
+// Bundle the service worker...
+const { count, size, warnings } = await injectManifest({
+    swSrc: "src/app/sw.ts",
+    swDest: "dist/sw.js",
+    globDirectory: "dist/static",
+});
+if (warnings.length > 0) {
+    console.warn("[@serwist/build] Oopsie, there are warnings from Serwist:", warnings);
+}
+console.log(`[@serwist/build] Manifest injected: ${count} files, totaling ${size} bytes.`);
